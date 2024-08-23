c7706d8d12284145d8caaaf0a7a4aceeed61400a
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-23 12:41:50 +0530

    ball get react function for collision detect created

--- jumper/ball.py
+++ jumper/ball.py
@@ -14,7 +14,7 @@ class Ball:
             self.on_ground = False
 
     def move(self):
-        self.dy += 5  # Gravity effect
+        self.dy += 3.5  # Gravity effect
         self.y += self.dy
 
         # Ball hits the ground
@@ -25,3 +25,6 @@ class Ball:
 
     def draw(self, screen):
         pygame.draw.circle(screen, BALL_COLOR, (self.x, self.y), BALL_RADIUS)
+
+    def get_rect(self):
+        return pygame.Rect(self.x - BALL_RADIUS, self.y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
