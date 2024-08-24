83495cc76be51ba65be53f59bee9e366722d8d2e
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-24 23:44:01 +0530

    image based asteroid created

--- navigator/obstacle.py
+++ navigator/obstacle.py
@@ -1,22 +1,20 @@
 import pygame
 import random
-from const import SCREEN_WIDTH, SCREEN_HEIGHT
+from const import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_IMAGE
 
 class Asteroid:
-    def __init__(self, x, y, size):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
-        self.size = size
-        self.color = (169, 169, 169)  # Gray color for asteroids
-        self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
+        self.image = pygame.image.load(ASTEROID_IMAGE)
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def move(self, speed):
         self.y += speed
         if self.y > SCREEN_HEIGHT:
-            self.y = -self.size
-            self.x = random.randint(0, SCREEN_WIDTH - self.size)
-
+            self.y = -self.rect.height
+            self.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
         self.rect.topleft = (self.x, self.y)
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+    def draw(self, screen, offset_x, offset_y):
+        screen.blit(self.image, (self.rect.x - offset_x, self.rect.y - offset_y))
