5c3f66315f5ebd7526a3235752e3a296d811e856
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-29 22:08:38 +0530

    variables added

---
+++ flappybird/const.py
@@ -0,0 +1,29 @@
+
+
+PIPE_COLOR = (36, 117, 47)
+BACKGROUND_COLOR = (33, 38, 38)
+FPS = 60
+
+
+BIRD_COLOR = (202, 252, 3)
+
+FLAP_STRENGTH = 3
+PIPE_SPAWN_FREQUENCY = 300
+
+# Screen settings
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BACKGROUND_COLOR = (135, 206, 235)  # Light sky blue
+
+# Bird settings
+BIRD_RADIUS = 15
+BIRD_COLOR = (255, 0, 0)  # Red
+GRAVITY = 0.5
+FLAP_STRENGTH = 10
+
+# Obstacle settings (pipes)
+PIPE_WIDTH = 80
+PIPE_GAP = 200  # Gap between the top and bottom pipes
+PIPE_SPEED = 3
+PIPE_GENERATE_INTERVAL = 1500  # Time (in milliseconds) between generating new pipes
+
