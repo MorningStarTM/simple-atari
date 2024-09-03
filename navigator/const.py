17616590b02dfffd7088d05245d5706665e9fcaf
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-09-03 20:35:07 +0530

    path error fixed

--- navigator/const.py
+++ navigator/const.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+root = os.getcwd()
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
@@ -13,5 +15,5 @@ JET_SPEED = 2
 BRAKE_SPEED = 3
 
 FPS = 60
-ASTEROID_IMAGES = [Path("navigator\\assets\\rock.png"), Path("navigator\\assets\\asteroid.png")]
-IMAGE = Path("navigator\\assets\\fighter-jet.png")
+ASTEROID_IMAGES = [Path(os.path.join(root, "navigator/assets/rock.png")), Path(os.path.join(root, "navigator/assets/asteroid.png"))]
+IMAGE = Path(os.path.join(root, "navigator/assets/fighter-jet.png"))
