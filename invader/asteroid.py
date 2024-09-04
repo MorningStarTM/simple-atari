a83c36842c7a3ba2a0bdaf1463c03691356506c2
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-09-04 21:45:01 +0530

    asteroid created

---
+++ invader/asteroid.py
@@ -0,0 +1,19 @@
+# asteroid.py
+import pygame
+import random
+from const import *
+
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Asteroid, self).__init__()
+        self.image = pygame.image.load(ASTEROID).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))  # Scale as needed
+        self.rect = self.image.get_rect(center=(x, y))
+        self.speed = random.randint(1, 4)
+
+    def update(self):
+        # Move asteroid down the screen
+        self.rect.y += self.speed
+        # Remove asteroid if it goes off screen
+        if self.rect.top > HEIGHT:
+            self.kill()
