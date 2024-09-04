ff26b47164246dcf9579e2dc2dcbeec330107a84
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-09-04 21:45:30 +0530

    bullet created

---
+++ invader/bullet.py
@@ -0,0 +1,17 @@
+# bullet.py
+import pygame
+from const import *
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Bullet, self).__init__()
+        self.image = pygame.image.load(BULLET).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (5, 8))  # Adjust size as needed
+        self.rect = self.image.get_rect(center=(x, y))
+        self.speed = -8
+
+    def update(self):
+        self.rect.y += self.speed
+        # Remove bullet if it goes off screen
+        if self.rect.bottom < 0:
+            self.kill()
