ba9638e46c2293de1ef00166c18846246a84693b
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-09-04 22:26:35 +0530

    path error added

--- invader/const.py
+++ invader/const.py
@@ -7,7 +7,7 @@ WIDTH = 800
 HEIGHT = 600
 
 
-ASTEROID = Path(os.path.join(root, 'assets/asteroid_2.png'))
-JET = Path(os.path.join(root, './assets/spaceship.png'))
-BULLET = Path(os.path.join(root, './assets/bullet.png'))
-BG = Path(os.path.join(root, './assets/bg.jpg'))
+ASTEROID = Path(os.path.join(root, 'invader/assets/asteroid_2.png'))
+JET = Path(os.path.join(root, 'invader/assets/spaceship.png'))
+BULLET = Path(os.path.join(root, 'invader/assets/bullet.png'))
+BG = Path(os.path.join(root, 'invader/assets/bg.jpg'))
