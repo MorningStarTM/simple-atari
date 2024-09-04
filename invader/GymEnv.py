8b77c1748f38fab516b32ca6ab5e5bebb101391e
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-09-04 22:11:06 +0530

    step func created

--- invader/GymEnv.py
+++ invader/GymEnv.py
@@ -30,3 +30,28 @@ class Invader(gym.Env):
         self.game.__init__()  # Reinitialize to reset game
         self.done = False
         return self._get_observation()
+    
+
+    def step(self, action):
+        # Apply action to the game
+        if action == 1:
+            self.screen.jet1.move_left()
+        elif action == 2:
+            self.screen.jet1.move_right()
+        elif action == 3:
+            self.screen.jet1.shoot(self.screen.bullet_group())
+
+        # Update the game state
+        self.screen.update()
+
+        # Check for collisions and update rewards
+        reward = self._calculate_reward()
+
+        # Check if the game is done
+        self.done = self._check_done()
+
+        # Get the next observation
+        observation = self._get_observation()
+
+        # Return the step information
+        return observation, reward, self.done, {}
